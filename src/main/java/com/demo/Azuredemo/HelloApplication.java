d561597d435d7c6653b975dbc4637fc168f8af98
unknown <ranadheerchinna92@gmail.com>
2021-06-27 19:38:01 -0500

    first deployment

---
+++ src/main/java/com/demo/Azuredemo/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.demo.Azuredemo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
